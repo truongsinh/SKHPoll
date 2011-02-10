ed000484c0f4d799255e751b10eb82731eacf246
TruongSinh Tran <truongsinh.tran@gmail.com>
2011-02-10 09:14:22 +0200

    First commit

---
+++ mypolls/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
+    was_published_today.short_description = 'Published today?'
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.CharField(max_length=200)
+    votes = models.IntegerField()
+
